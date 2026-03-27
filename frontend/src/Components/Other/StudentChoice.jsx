cfa6ba28c152aa2e53f7606ec73ba2e9f6fb82cf
Messai Hailemariam <105058702+hmmessai@users.noreply.github.com>
2026-03-27 09:19:53 +0000

    profile build

--- frontend/src/Components/Other/StudentChoice.jsx
+++ frontend/src/Components/Other/StudentChoice.jsx
@@ -10,6 +10,7 @@ import { AuthContext } from "../Auth/AuthContext";
 export default function StudentChoice( props ) {
     const [students, setStudents] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [student, setStudent] = useState({});
     
 
     useEffect(() => {
@@ -47,18 +48,66 @@ export default function StudentChoice( props ) {
         fetchStudents();
     }, [loading]);
 
-    const getAttendance = () => {
-    }
+    const getProfile = async (id) => {
+        try {
+            setLoading(true);
+            const response = await axiosInstance.get(endPoint.GETSTUDENT,
+                {
+                    id: id,
+                },
+                {
+                    headers: {
+                        "Authorization": `Bearer ${Cookies.get("token")}`,
+                        "Content-Type": "application/json"
+                    }
+                });
+            
+            console.log(response);
+            setStudent(response.data);
+        } catch (err) {
+            console.log("Error fetching student Profile:", err);
+            if (err.response && err.response.status === 404) {
+                setStudents([]);
+                toast.warning("Student profile could not be fetched");
+            } else if (err.response && err.response.status === 401) {
+                toast.error("Unauthorized. Please log in again.");
+            } else {
+                toast.error("Internal Server Error");
+            }
+        } finally {
+            setLoading(false);
+        }
+    };
 
     return (
-        <div className="d-flex flex-column align-items-center justify-content-center" style={{ minHeight: "60vh" }}>
-            <h2 className="mb-4">Select a Student</h2>
-            {students.map((student, index) => (
-                <button key={index} className="btn btn-dark w-50 m-2" onClick={getAttendance()}>
-                    {student.name}
-                </button>
-            ))}
-            <div></div>
+        <div className="position-relative">
+            {loading && (
+                <div className="loading-overlay">
+                <div className="spinner-border text-light"></div>
+                </div>
+            )}
+            <div className="d-flex flex-column align-items-center justify-content-center" style={{ minHeight: "60vh", display: !student ? 'none': 'show' }}>
+                <h2 className="mb-4">Select a Student</h2>
+                {students.map((student, index) => (
+                    <button key={student.id} className="btn btn-dark w-50 m-2" onClick={getProfile(student.id)}>
+                        {student.name}
+                    </button>
+                ))}
+                <div 
+                className="d-flex flex-row align-items-center justify-content-center"
+                style={{ display: student ? 'none' : 'flex' }}
+                >
+                    <h1>{student.name}</h1>
+                    {student.attendance.map((att, index) => {
+                        <div>
+                            <p>{index}</p>
+                            <p>{att.date}</p>
+                        </div>
+                        
+                    })}
+                </div>
+            </div>
         </div>
+        
     );
 }
